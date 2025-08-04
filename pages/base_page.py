f603b8306e2ef547c85db12fdc4ff4b92008f961
Philipp Lazarev <philipp.lazareff@gmail.com>
2025-08-04 14:37:47 +0300

    Added conftest and test_main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,10 @@ import time
 import os 
 import math
 
-class BasePage:
+class BasePage():
     def __init__(self, browser, url):
-    self.browser = browser
-    self.url = url
+        self.browser = browser
+        self.url = url
+
+    def open(self): 
+        self.browser.get(self.url)
