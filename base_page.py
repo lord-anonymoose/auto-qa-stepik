6b778e9762dd3b259fb18fef4ed6b64ffefdb4d1
Philipp Lazarev <philipp.lazareff@gmail.com>
2025-08-04 14:28:42 +0300

    first commit: base_page.py created

---
+++ base_page.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os 
+import math
+
+class BasePage:
+    def __init__(self, browser, url):
+    self.browser = browser
+    self.url = url
